4c0a22da4ee512cadc7e738c8a32a29903751786
Oleh Bohdan <oleh.bohdan@imprivata.com>
2023-09-05 15:44:12 +0300

    UC#10 Implement regex tests

--- app/src/main/java/com/example/usecase10/StringUtils.kt
+++ app/src/main/java/com/example/usecase10/StringUtils.kt
@@ -4,8 +4,8 @@ object StringUtils {
     const val ONE_UPPERCASE = """(?=.*[A-Z])"""
     const val ONE_LOWERCASE = """(?=.*[a-z])"""
     const val ONE_DIGIT = """(?=.*\d)"""
-    const val ONE_SPECIAL_CHAR = """(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])"""
-    const val NO_WHITESPACE = """(?![\s\t\n\r])"""
+    const val ONE_SPECIAL_CHAR = """(?=.*[!"#$%&'()*+,-./:;<=>?@\[\\\]^_`{|}~])"""
+    const val NO_WHITESPACE = """(?!.*[\s\t\n\r])"""
 
     fun isValid(input: String, maxLength: Int): Boolean {
         // Ensure string is at most 'maxLength' characters
--- app/src/test/java/com/example/usecase10/ExampleUnitTest.kt
+++ app/src/test/java/com/example/usecase10/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.usecase10
 
+import com.example.usecase10.StringUtils.isValid
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -11,7 +12,73 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testValidString() {
+        val input = "Aa1!"
+        assertTrue("String should be valid.", isValid(input, 10))
+    }
+
+    @Test
+    fun testNoUppercase() {
+        val input = "aa1!"
+        assertFalse("String should be invalid as it doesn't contain an uppercase letter.", isValid(input, 10))
+    }
+
+    @Test
+    fun testNoLowercase() {
+        val input = "AA1!"
+        assertFalse("String should be invalid as it doesn't contain a lowercase letter.", isValid(input, 10))
+    }
+
+    @Test
+    fun testNoDigit() {
+        val input = "Aa!!"
+        assertFalse("String should be invalid as it doesn't contain a digit.", isValid(input, 10))
+    }
+
+    @Test
+    fun testNoSpecialChar() {
+        val input = "Aa11"
+        assertFalse("String should be invalid as it doesn't contain a special character.", isValid(input, 10))
+    }
+
+    @Test
+    fun testContainsWhitespace() {
+        val input = "Aa 1!"
+        assertFalse("String should be invalid as it contains whitespace.", isValid(input, 10))
+    }
+
+    @Test
+    fun testMaxLengthExceeded() {
+        val input = "Aa1!Aa1!Aa1"
+        assertFalse("String should be invalid as it exceeds max length.", isValid(input, 10))
+    }
+
+    @Test
+    fun testExactlyAtMaxLength() {
+        val input = "Aa1!Aa1!Aa"
+        assertTrue("String should be valid as it's exactly at the max length.", isValid(input, 10))
+    }
+
+    @Test
+    fun testSpecialCharacters() {
+        val input = "Aa1@#$%^&*"
+        assertTrue("String should be valid with various special characters.", isValid(input, 10))
+    }
+
+    @Test
+    fun testEdgeCaseEmptyString() {
+        val input = ""
+        assertFalse("String should be invalid as it's empty.", isValid(input, 10))
+    }
+
+    @Test
+    fun testContainsVariousWhitespaceCharacters() {
+        val spaceInput = "Aa1 !"
+        val tabInput = "Aa1!\t"
+        val newlineInput = "Aa1!\n"
+
+        assertFalse("String should be invalid as it contains a space.", isValid(spaceInput, 10))
+        assertFalse("String should be invalid as it contains a tab.", isValid(tabInput, 10))
+        assertFalse("String should be invalid as it contains a newline.", isValid(newlineInput, 10))
     }
 }
